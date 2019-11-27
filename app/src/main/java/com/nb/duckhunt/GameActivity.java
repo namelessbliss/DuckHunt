f185bdbec870e97012e5a73aad7465b033eef03c
namelessbliss <anonimo2911.2012@gmail.com>
2019-11-26 23:24:53 -0500

    Pasando nick de jugador a vista de juego

--- app/src/main/java/com/nb/duckhunt/GameActivity.java
+++ app/src/main/java/com/nb/duckhunt/GameActivity.java
@@ -1,14 +1,29 @@
 package com.nb.duckhunt;
 
+import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.nb.duckhunt.Common.Constantes;
 
 public class GameActivity extends AppCompatActivity {
+
+    private TextView tvCounter, tvTimer, tvNick;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        tvCounter = findViewById(R.id.tvCounter);
+        tvTimer = findViewById(R.id.tvTimer);
+        tvNick = findViewById(R.id.tvNick);
+
+        // Obtiene el nick del usuario
+        Bundle extras = getIntent().getExtras();
+        String nick = extras.getString(Constantes.EXTRA_NICK);
+        tvNick.setText(nick);
+
     }
 }
