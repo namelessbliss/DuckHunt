5e878a505bd9a69ba2369ead8e88259fffdc589c
namelessbliss <anonimo2911.2012@gmail.com>
2019-11-26 23:33:17 -0500

    Evento clic sobre pato

--- app/src/main/java/com/nb/duckhunt/GameActivity.java
+++ app/src/main/java/com/nb/duckhunt/GameActivity.java
@@ -2,6 +2,8 @@ package com.nb.duckhunt;
 
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,18 +13,26 @@ import com.nb.duckhunt.Common.Constantes;
 public class GameActivity extends AppCompatActivity {
 
     private TextView tvCounter, tvTimer, tvNick;
+    private ImageView ivDuck;
+    private int counter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
+        initViewComponents();
+        eventos();
+    }
+
+    private void initViewComponents() {
         tvCounter = findViewById(R.id.tvCounter);
         tvTimer = findViewById(R.id.tvTimer);
         tvNick = findViewById(R.id.tvNick);
+        ivDuck = findViewById(R.id.ivDuck);
 
         //cambiar tipo de fuente
-        Typeface typeface = Typeface.createFromAsset(getAssets(),"pixel.ttf");
+        Typeface typeface = Typeface.createFromAsset(getAssets(), "pixel.ttf");
         tvCounter.setTypeface(typeface);
         tvTimer.setTypeface(typeface);
         tvNick.setTypeface(typeface);
@@ -32,5 +42,15 @@ public class GameActivity extends AppCompatActivity {
         String nick = extras.getString(Constantes.EXTRA_NICK);
         tvNick.setText(nick);
 
+    }
+
+    private void eventos() {
+        ivDuck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                counter++;
+                tvCounter.setText(counter + "");
+            }
+        });
     }
 }
