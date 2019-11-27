820a2180040d830256525ce2aad6a40aa762f4be
namelessbliss <anonimo2911.2012@gmail.com>
2019-11-27 10:51:39 -0500

    Generacion aleatoria de pato

--- app/src/main/java/com/nb/duckhunt/GameActivity.java
+++ app/src/main/java/com/nb/duckhunt/GameActivity.java
@@ -1,7 +1,10 @@
 package com.nb.duckhunt;
 
+import android.graphics.Point;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Display;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -10,11 +13,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.nb.duckhunt.Common.Constantes;
 
+import java.util.Random;
+
 public class GameActivity extends AppCompatActivity {
 
     private TextView tvCounter, tvTimer, tvNick;
     private ImageView ivDuck;
     private int counter;
+    private int anchoPantalla, altoPantalla;
+
+    Random random;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +31,20 @@ public class GameActivity extends AppCompatActivity {
         setContentView(R.layout.activity_game);
 
         initViewComponents();
+        initPantalla();
         eventos();
+    }
+
+    private void initPantalla() {
+        // Obtiene tamaño de pantall
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        anchoPantalla = size.x;
+        altoPantalla = size.y;
+
+        //objeto para generar numeros random
+        random = new Random();
     }
 
     private void initViewComponents() {
@@ -50,7 +72,32 @@ public class GameActivity extends AppCompatActivity {
             public void onClick(View view) {
                 counter++;
                 tvCounter.setText(counter + "");
+                ivDuck.setImageResource(R.drawable.duck_clicked);
+                ivDuck.setClickable(false);
+
+                // Ejecutar codigo segundos despues
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        ivDuck.setImageResource(R.drawable.duck);
+                        moveDuck();
+                    }
+                }, 500);
             }
         });
+    }
+
+    private void moveDuck() {
+        int min = 0;
+        int maximoX = anchoPantalla - ivDuck.getWidth();
+        int maxY = altoPantalla - ivDuck.getHeight();
+
+        //numeros aleatorios
+        int randomX = random.nextInt(((maximoX - min) + 1) + min);
+        int randomY = random.nextInt(((maxY - min) + 1) + min);
+
+        ivDuck.setX(randomX);
+        ivDuck.setY(randomY);
+        ivDuck.setClickable(true);
     }
 }
